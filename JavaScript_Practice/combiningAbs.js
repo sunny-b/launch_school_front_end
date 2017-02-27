d89f4a77579fdf55f2e45a03949ab699a66b789c
Sunny <sjbeatteay@gmail.com>
2017-02-27 12:16:01 -0500

    part way through Student Grade Summay

--- JavaScript_Practice/combiningAbs.js
+++ JavaScript_Practice/combiningAbs.js
@@ -111,37 +111,184 @@
 
 // Bands
 
-var bands = [
-  { name: 'sunset rubdown', country: 'UK', active: false },
-  { name: 'women', country: 'Germany', active: false },
-  { name: 'a silver mt. zion', country: 'Spain', active: true },
-];
+// var bands = [
+//   { name: 'sunset rubdown', country: 'UK', active: false },
+//   { name: 'women', country: 'Germany', active: false },
+//   { name: 'a silver mt. zion', country: 'Spain', active: true },
+// ];
+//
+// function processBands(data) {
+//   function removePeriod(string) {
+//     return string.replace(/\./g, '');
+//   }
+//
+//   function capitalize(string) {
+//     return string.split(' ').map(function(word) {
+//       return word[0].toUpperCase() + word.slice(1);
+//     }).join(' ');
+//   }
+//
+//   function format(bandName) {
+//     var formattedName = removePeriod(bandName);
+//     formattedName = capitalize(formattedName);
+//
+//     return formattedName;
+//   }
+//
+//   return data.map(function(band) {
+//     return {
+//       name: format(band.name),
+//       country: 'Canada',
+//       active: band.active,
+//     }
+//   });
+// }
+//
+// var answer = processBands(bands);
 
-function processBands(data) {
-  function removePeriod(string) {
-    return string.replace(/\./g, '');
+
+
+// Student Grades
+
+var studentScores = {
+  student1: {
+    id: 123456789,
+    scores: {
+      exams: [90, 95, 100, 80],
+      exercises: [20, 15, 10, 19, 15],
+    },
+  },
+  student2: {
+    id: 123456799,
+    scores: {
+      exams: [50, 70, 90, 100],
+      exercises: [0, 15, 20, 15, 15],
+    },
+  },
+  student3: {
+    id: 123457789,
+    scores: {
+      exams: [88, 87, 88, 89],
+      exercises: [10, 20, 10, 19, 18],
+    },
+  },
+  student4: {
+    id: 112233445,
+    scores: {
+      exams: [100, 100, 100, 100],
+      exercises: [10, 15, 10, 10, 15],
+    },
+  },
+  student5: {
+    id: 112233446,
+    scores: {
+      exams: [50, 80, 60, 90],
+      exercises: [10, 0, 10, 10, 0],
+    },
+  },
+};
+
+function generateClassRecordSummary(scores) {
+  function extractStudentData(scores, data) {
+    Object.keys(scores).forEach(function(key) {
+      var student = data['studentScores'][key];
+      var studentScores = scores[key]['scores'];
+
+      student['examAvg'] = calcStudentExamAvg(studentScores);
+      student['exerciseTotal'] = calcExerciseTotal(studentScores);
+      populateFields(data['exams'], studentScores);
+    });
+  }
+  function calcStudentExamAvg(studentScores) {
+    var examScores = studentScores['exams'];
+    var examTotal = examScores.reduce(function(acc, score) {
+      return acc + score;
+    });
+
+    return Math.round((examTotal / examScores.length) * 100) / 100;
+  }
+  function calcExerciseTotal(studentScores) {
+    var exerciseScores = studentScores['exercises'];
+    var exerciseTotal = exerciseScores.reduce(function(acc, score) {
+      return acc + score;
+    });
+
+    return exerciseTotal;
+  }
+  function populateFields(exams, studentScores) {
+    Object.keys(exams).forEach(function(exam, index) {
+      exams[exam].push(studentScores['exams'][index]);
+    });
   }
 
-  function capitalize(string) {
-    return string.split(' ').map(function(word) {
-      return word[0].toUpperCase() + word.slice(1);
-    }).join(' ');
+  function processStudentData(data) {
+
   }
 
-  function format(bandName) {
-    var formattedName = removePeriod(bandName);
-    formattedName = capitalize(formattedName);
+  var data = {
+    exams: {
+      exam1: [],
+      exam2: [],
+      exam3: [],
+      exam4: [],
+    },
 
-    return formattedName;
-  }
+    studentScores: {
+      student1: {
+        examAvg: null,
+        exerciseTotal: null,
+        finalGrade: null,
+      },
+      student2: {
+        examAvg: null,
+        exerciseTotal: null,
+        finalGrade: null,
+      },
+      student3: {
+        examAvg: null,
+        exerciseTotal: null,
+        finalGrade: null,
+      },
+      student4: {
+        examAvg: null,
+        exerciseTotal: null,
+        finalGrade: null,
+      },
+      student5: {
+        examAvg: null,
+        exerciseTotal: null,
+        finalGrade: null,
+      },
+    },
+  };
 
-  return data.map(function(band) {
-    return {
-      name: format(band.name),
-      country: 'Canada',
-      active: band.active,
-    }
-  });
-}
+  extractStudentData(scores, data);
+  processStudentData(data);
+  renderSummary(data);
+  //
+  // sortedExams = exams.sort(function(exam1, exam2) {
+  //   if (exam1.average < exam2.average) {
+  //     return -1;
+  //   } else if (exam1.average > exam2.average) {
+  //     return 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // });
+  //
+  // return exams;
+};
 
-var answer = processBands(bands);
+var answer = generateClassRecordSummary(studentScores);
+
+// returns:
+//
+// {
+//   studentGrades: [ '87 (B)', '73 (D)', '84 (C)', '86 (B)', '56 (F)' ],
+//   exams: [
+//     { average: 75.6, minimum: 50, maximum: 100 },
+//     { average: 86.4, minimum: 70, maximum: 100 },
+//     { average: 87.6, minimum: 60, maximum: 100 },
+//     { average: 91.8, minimum: 80, maximum: 100 },
+//   ],
+// }
