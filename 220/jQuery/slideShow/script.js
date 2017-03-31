e246f6accadea0f8042e7712a9eeca7fcb3b5b4f
Sun-Li Beatteay <sjbeatteay@gmail.com>
2017-03-31 19:53:24 -0400

    finish jQuery slideshow assignment

--- 220/jQuery/slideShow/script.js
+++ 220/jQuery/slideShow/script.js
@@ -1,15 +1,54 @@
 $(function() {
   $('.main-photos img').eq(0).show();
-
+  $('.left').hide();
 
   $('.thumbnails a').click(function(e) {
     e.preventDefault();
-    var $clickList = $(this).parent();
-    var index = $('.thumbnails li').index($clickList);
+    var $clickedItem = $(this).parent();
+    var $thumbnails = $('.thumbnails li');
+    var index = $thumbnails.index($clickedItem);
 
-    $('.thumbnails li').siblings().find('img').removeClass('active');
+    $thumbnails.siblings().find('img').removeClass('active');
     $(this).find('img').addClass('active');
 
-    $('.main-photos img').stop().fadeOut(500).delay(500).eq(index).fadeIn(500);
+    $('.main-photos img').stop()
+                         .removeClass('show')
+                         .fadeOut(500)
+                         .delay(500)
+                         .eq(index)
+                         .addClass('show')
+                         .fadeIn(500);
+
+    showArrows(index, $thumbnails.length);
   });
+
+  $('.right').click(newPic('next'));
+  $('.left').click(newPic('prev'));
+
+  function newPic(string) {
+    var $nextPic;
+
+    return function() {
+      if (string === 'next') {
+        $nextPic = $('.show').parent().next();
+      } else if (string === 'prev') {
+        $nextPic = $('.show').parent().prev();
+      }
+
+      var $thumbnails = $('.slideshow li');
+      var index = $thumbnails.index($nextPic);
+
+      $('.thumbnails a').eq(index).trigger('click');
+    }
+  }
+
+  function showArrows(index, maxLength) {
+    if (index === 0) {
+      $('.left').hide();
+    } else if (index === maxLength - 1) {
+      $('.right').hide();
+    } else {
+      $('.left, .right').show();
+    }
+  }
 });
